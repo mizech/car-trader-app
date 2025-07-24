c469cb161c3fa02c52d4c815feb0523d8fa3cafd
mizech <michael.zech@hotmail.de>
2025-07-24 13:07:08 +0200

    Impl.: Create car-form

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,23 @@
 class CarsController < ApplicationController
+  def index
+    @cars = Car.all
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    puts " -- Car#create --"
+    @car = Car.create(set_params)
+    if @car.save
+      redirect_to cars_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def set_params
+    params.expect(car: [ :name, :price, :vendor_id ])
+  end
 end
