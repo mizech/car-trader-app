a4ef02634d1c5f0d61adcfd904ba7200f3ac3d57
mizech <michael.zech@hotmail.de>
2025-07-24 09:58:50 +0200

    Impl.: Vendor edit

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,12 +1,21 @@
-require "debug"
-
 class VendorsController < ApplicationController
+  before_action :set_vendor, only: [ :show, :destroy, :edit, :update ]
   def index
     @vendors = Vendor.all
   end
 
   def show
-    @vendor = Vendor.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @vendor.update(set_params)
+      redirect_to vendor_path(@vendor)
+    else
+      redirect_to :edit, status: :unprocessable_entity
+    end
   end
 
   def new
@@ -20,14 +29,19 @@ class VendorsController < ApplicationController
       redirect_to root_path
       flash[:notice] = "Vendor successfully created."
     else
+      flash[:notice] = "Vendor creation failed."
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @vendor = Vendor.find(params[:id])
     @vendor.destroy
+    flash[:notice] = "Vendor has become deleted."
     redirect_to root_path
+  end
+
+  def set_vendor
+    @vendor = Vendor.find(params[:id])
   end
 
   def set_params
