fc6660dc860356d9e74f6754c312a1de856bdc73
mizech <michael.zech@hotmail.de>
2025-07-24 09:31:10 +0200

    Impl: Delete Vendor

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,3 +1,5 @@
+require "debug"
+
 class VendorsController < ApplicationController
   def index
     @vendors = Vendor.all
@@ -5,5 +7,30 @@ class VendorsController < ApplicationController
 
   def show
     @vendor = Vendor.find(params[:id])
+  end
+
+  def new
+    @vendor = Vendor.new
+  end
+
+  def create
+    @vendor = Vendor.new(set_params)
+
+    if @vendor.save
+      redirect_to root_path
+      flash[:notice] = "Vendor successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @vendor = Vendor.find(params[:id])
+    @vendor.destroy
+    redirect_to root_path
+  end
+
+  def set_params
+    params.expect(vendor: [ :name, :country ])
   end
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,3 +1,5 @@
 class Vendor < ApplicationRecord
   has_many :cars, dependent: :destroy
+
+  validates :name, presence: true
 end
